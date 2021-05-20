80758a8f84a57bb1cefedc0b32aba23126f7f8b7
Anthony NAUDIN <nd.anthony77@gmail.com>
2021-05-20 15:54:44 +0200

    dev connexion + backoffice

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -71,6 +71,11 @@ class User implements UserInterface
      */
     public $confirm_password;
 
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
     public function getId(): ?int
     {
         return $this->id;
@@ -148,6 +153,14 @@ class User implements UserInterface
 
     public function getRoles()
     {
-        return ['ROLE_USER'];
+        // return ['ROLE_USER'];
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
     }
 }
